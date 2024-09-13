af6bdcd85159cb3fc9ceb977f5d10ce7575ecdd9
Calvin Li <65045619+calvin-li-developer@users.noreply.github.com>
2024-09-13 00:19:32 -0400

    Update ffmpeg_fast_start.bat

--- windows_ffmpeg_scripts/ffmpeg_fast_start.bat
+++ windows_ffmpeg_scripts/ffmpeg_fast_start.bat
@@ -1,4 +1,5 @@
 if not exist "output" mkdir output
-for %%A IN (*.mp4) DO ffmpeg -i "%%A" -c copy -map 0 -movflags +faststart "output/%%A"
+for %%A IN (*) DO ffmpeg -i "%%A" -c copy -map 0 -metadata title="" -movflags +faststart "output/%%A"
 move /y "output\*" "./"
 rmdir /s /q "output"
+del "%~f0"
