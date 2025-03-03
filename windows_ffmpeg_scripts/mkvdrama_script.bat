6b22dcd31ead1e43562aceafe2507c0105459088
Calvin Li <65045619+calvin-li-developer@users.noreply.github.com>
2025-03-02 22:12:24 -0500

    Update mkvdrama_script.bat

--- windows_ffmpeg_scripts/mkvdrama_script.bat
+++ windows_ffmpeg_scripts/mkvdrama_script.bat
@@ -5,7 +5,7 @@ set i=0
 
 for %%F in (*.mkv) do (
   set /a i+=1
-  ffmpeg -i "%%F" -map_metadata -1 -c:v copy -c:a copy -metadata title="Episode !i!" -metadata:s:a:0 language=chi -c:s mov_text -map 0 -movflags +faststart "output/%%~nF.mp4" || (
+  ffmpeg -i "%%F" -c:v copy -c:a copy -metadata title="Episode !i!" -metadata:s:a:0 language=chi -c:s mov_text -map 0 -map_metadata 0 -movflags +faststart "output/%%~nF.mp4" || (
     echo An error occurred during processing. Exiting script.
     exit /b 1
   )
