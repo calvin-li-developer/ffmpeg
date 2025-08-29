c24aba1d65d36d083768aa52a5dea3e15f8484b5
Calvin Li <65045619+calvin-li-developer@users.noreply.github.com>
2025-08-29 13:13:53 -0400

    Update mkv_to_mp4.bat

--- windows_ffmpeg_scripts/mkv_to_mp4.bat
+++ windows_ffmpeg_scripts/mkv_to_mp4.bat
@@ -1,8 +1,9 @@
 if not exist "output" mkdir output
-for %%A IN (*.m*) DO ffmpeg -i "%%A" -c:v copy -c:a copy -movflags +faststart "output/%%~nA.mp4"
+for %%A IN (*.m*) DO ffmpeg -i "%%A" -map 0 -c:v copy -c:a copy -c:s mov_text -movflags +faststart "output/%%~nA.mp4"
 move /y "output\*" "./"
 rmdir /s /q "output"
 
 for %%i in (*.mkv) DO (
     del "%%i"
+
 )
