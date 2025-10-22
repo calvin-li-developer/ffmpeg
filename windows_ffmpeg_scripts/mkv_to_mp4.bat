ea40e105bde5ef51358cda3eea1665516cca9c97
Calvin Li <65045619+calvin-li-developer@users.noreply.github.com>
2025-10-21 20:14:05 -0400

    Enhance mkv_to_mp4.bat for better stream mapping

--- windows_ffmpeg_scripts/mkv_to_mp4.bat
+++ windows_ffmpeg_scripts/mkv_to_mp4.bat
@@ -1,5 +1,8 @@
+@echo off
 if not exist "output" mkdir output
-for %%A IN (*.m*) DO ffmpeg -i "%%A" -map 0 -c:v copy -c:a copy -c:s mov_text -movflags +faststart "output/%%~nA.mp4"
+for %%A in (*.m*) do (
+    ffmpeg -i "%%A" -map 0:v -map 0:a -map 0:s -c:v copy -c:a copy -c:s mov_text -movflags +faststart "output/%%~nA.mp4"
+)
 move /y "output\*" "./"
 rmdir /s /q "output"
 
@@ -7,3 +10,4 @@ for %%i in (*.mkv) DO (
     del "%%i"
 
 )
+
