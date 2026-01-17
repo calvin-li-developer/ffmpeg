888ec1dc06f5a13436719e3dae9a72b72fa5cdd7
Calvin Li <65045619+calvin-li-developer@users.noreply.github.com>
2026-01-16 23:53:14 -0500

    Update mkvdrama_script.bat

--- windows_ffmpeg_scripts/mkvdrama_script.bat
+++ windows_ffmpeg_scripts/mkvdrama_script.bat
@@ -1,11 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 if not exist "output" mkdir output
-set i=0
 
 for %%F in (*.mkv) do (
-  set /a i+=1
-  ffmpeg -i "%%F" -c:v copy -c:a copy -metadata title="Episode !i!" -metadata:s:a:0 language=chi -c:s mov_text -map 0 -map_metadata 0 -movflags +faststart "output/%%~nF.mp4" || (
+  ffmpeg -i "%%F" -c:v copy -c:a copy -metadata title="" -metadata:s:a language=chi -c:s mov_text -map 0 -map_metadata 0 -movflags +faststart "output/%%~nF.mp4" || (
     echo An error occurred during processing. Exiting script.
     exit /b 1
   )
@@ -17,3 +15,4 @@ rmdir /s /q "output"
 del /q *.mkv
 del "%~f0"
 endlocal
+
